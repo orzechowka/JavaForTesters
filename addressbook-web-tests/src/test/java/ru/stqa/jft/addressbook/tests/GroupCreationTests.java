d998f226130f867273e8ce220a1b261d86130c0b
a.warzecha <a.warzecha89@gmail.com>
2016-12-28 23:37:11 +0100

    Lambda, contacts

--- addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/GroupCreationTests.java
@@ -18,7 +18,7 @@ public class GroupCreationTests extends TestBase {
         app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
-        
+
         before.add(group);
         group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
