3cba725984d501ae541c10e8a3c23c444c31f89e
a.warzecha <a.warzecha89@gmail.com>
2017-01-05 21:42:09 +0100

    Common usage of ids (gropus)

--- addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/model/GroupData.java
@@ -6,6 +6,23 @@ public class GroupData {
     private String name;
     private String header;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GroupData groupData = (GroupData) o;
+
+        if (id != groupData.id) return false;
+        return name != null ? name.equals(groupData.name) : groupData.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
 
     public GroupData withId(int id) {
         this.id = id;
@@ -29,25 +46,6 @@ public class GroupData {
     }
 
     private String footer;
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GroupData groupData = (GroupData) o;
-
-        return name != null ? name.equals(groupData.name) : groupData.name == null;
-    }
-
-    @Override
-    public int hashCode() {
-        return name != null ? name.hashCode() : 0;
-    }
-
-
-
 
 
     public String getName() {
