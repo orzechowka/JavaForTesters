9c06b65bb8d07746caf09dd17cf57fc25e9317fc
a.warzecha <a.warzecha89@gmail.com>
2017-01-14 23:27:00 +0100

    creating log files

--- addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/jft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,8 @@ package ru.stqa.jft.addressbook.tests;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.jft.addressbook.model.GroupData;
@@ -20,6 +22,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
+
 
     @DataProvider
     public Iterator<Object[]> validGroupsFromXml() throws IOException {
