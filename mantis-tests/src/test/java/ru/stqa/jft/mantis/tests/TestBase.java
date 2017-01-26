0c5c0cd993fa4ea77e59fb00ff17dc636ebab24b
a.warzecha <a.warzecha89@gmail.com>
2017-01-26 21:44:13 +0100

    New module added (Mantis)

---
+++ mantis-tests/src/test/java/ru/stqa/jft/mantis/tests/TestBase.java
@@ -0,0 +1,28 @@
+package ru.stqa.jft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.jft.mantis.appmanager.ApplicationManager;
+
+
+/**
+ * Created by Anna on 2016-12-10.
+ */
+public class TestBase {
+
+
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite(alwaysRun =  true)
+    public void tearDown() {
+        app.stop();
+    }
+
+}
