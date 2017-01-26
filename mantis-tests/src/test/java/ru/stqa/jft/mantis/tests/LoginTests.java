ce203042ea8e905933e28fefe753a8f2e172fe01
a.warzecha <a.warzecha89@gmail.com>
2017-01-26 22:49:16 +0100

    HttpSession and LoginTests added

---
+++ mantis-tests/src/test/java/ru/stqa/jft/mantis/tests/LoginTests.java
@@ -0,0 +1,22 @@
+package ru.stqa.jft.mantis.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.jft.mantis.appmanager.HttpSession;
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Created by Anna on 2017-01-26.
+ */
+public class LoginTests extends TestBase {
+
+    @Test
+    public void testLogin() throws IOException {
+        HttpSession session = app.newSession();
+        assertTrue(session.login("administrator", "root"));
+        assertTrue(session.isLoggedInAs("administrator"));
+    }
+}
