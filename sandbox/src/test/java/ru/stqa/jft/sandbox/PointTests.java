f0b6073ee7a7f56cd7e33275c69fbed71d69040f
a.warzecha <a.warzecha89@gmail.com>
2017-02-09 23:21:18 +0100

    break tests2

--- sandbox/src/test/java/ru/stqa/jft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/jft/sandbox/PointTests.java
@@ -19,7 +19,7 @@ public class PointTests {
     public void testPoint1() {
         Point p1 = new Point(9, 5);
         Point p2 = new Point(4, 5);
-        Assert.assertEquals(p1.distance(p2), 5.0);
+        Assert.assertEquals(p1.distance(p2), 3.0);
     }
 
     @Test
